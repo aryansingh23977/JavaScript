5341a0ed521762f7d6adfc562e324ed46e2aa7dd
ASCODES <aryansingh23977@gmail.com>
2026-05-18 17:55:52 +0000

    Strings in JavaScript

--- My JS Notes/strings.js
+++ My JS Notes/strings.js
@@ -1,7 +1,7 @@
 let name="Aryan";
 let repoCount=50;
 //console.log(name+repoCount+"Value");
-//console.log(`Hello my name is ${name} and my repocount is ${repoCount}. TY xD`);
+//console.log(`Hello my name is ${name} and my repocount is ${repoCount}. TY xD`); //also known as string interpolation
 const Name ='hello'; //string
 console.log(typeof Name);
 const Name2;
